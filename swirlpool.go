15036f2f8b6cf3789605646389e4ccfff609f3b1
Ivan Tam <ivan@hipnik.net>
2015-02-10 22:38:38 -0800

    Handle conn closes; Message struct
    * Unregister Connections when an HTTP connection closes
    * Use new Message type instead of []byte
    * Print debug message to stdout

--- swirlpool.go
+++ swirlpool.go
@@ -9,15 +9,42 @@ import (
 	"time"
 )
 
+type Message struct {
+	event string
+	data  string
+	id    string
+}
+
+func (m *Message) Bytes() []byte {
+	var buf bytes.Buffer
+
+	if m.event != "" {
+		buf.Write([]byte("event: "))
+		buf.Write([]byte(m.event))
+	}
+
+	buf.Write([]byte("data: "))
+	buf.Write([]byte(m.data))
+
+	if m.id != "" {
+		buf.Write([]byte("id: "))
+		buf.Write([]byte(m.id))
+	}
+
+	buf.Write([]byte("\n\n"))
+
+	return buf.Bytes()
+}
+
 type Hub struct {
 	connections map[*Connection]bool // Registered connections.
-	broadcast   chan []byte          // Inbound messages from the connections.
+	broadcast   chan Message         // Inbound messages from the connections.
 	register    chan *Connection     // Register requests from the connections.
 	unregister  chan *Connection     // Unregister requests from connections.
 }
 
 var h = Hub{
-	broadcast:   make(chan []byte),
+	broadcast:   make(chan Message),
 	register:    make(chan *Connection),
 	unregister:  make(chan *Connection),
 	connections: make(map[*Connection]bool),
@@ -27,10 +54,10 @@ func (h *Hub) Run() {
 	for {
 		select {
 		case c := <-h.register:
-			fmt.Sprintf("Register connection: %s (%s)\n", c, len(h.connections))
+			fmt.Printf("Register connection: %s (%s)\n", c, len(h.connections))
 			h.connections[c] = true
 		case c := <-h.unregister:
-			fmt.Sprintf("Unregister connection: %s (%s)\n", c, len(h.connections))
+			fmt.Printf("Unregister connection: %s (%s)\n", c, len(h.connections))
 			if _, ok := h.connections[c]; ok {
 				delete(h.connections, c)
 				close(c.messageChan)
@@ -49,34 +76,7 @@ func (h *Hub) Run() {
 }
 
 type Connection struct {
-	messageChan chan []byte
-}
-
-type Message struct {
-	event string
-	data  string
-	id    string
-}
-
-func (m *Message) Bytes() {
-	var buf bytes.Buffer
-
-	if m.event {
-		buf.Write([]byte("event: "))
-		buf.Write([]byte(m.event))
-	}
-
-	buf.Write([]byte("data: "))
-	buf.Write([]byte(m.data))
-
-	if m.id {
-		buf.Write([]byte("id: "))
-		buf.Write([]byte(m.id))
-	}
-
-	buf.Write("\n\n")
-
-	return buf.Bytes()
+	messageChan chan Message
 }
 
 func broadcaster() {
@@ -88,10 +88,11 @@ func broadcaster() {
 		cmd.Stderr = &buf
 		cmd.Run()
 
-		h.broadcast <- buf.Bytes()
+		m := Message{data: buf.String()}
+		h.broadcast <- m
 		buf.Reset()
 
-		time.Sleep(1 * time.Second)
+		time.Sleep(2 * time.Second)
 	}
 }
 
@@ -99,18 +100,22 @@ func ClientHandler(w http.ResponseWriter, r *http.Request) {
 	var b bytes.Buffer
 	flusher := w.(http.Flusher)
 
-	conn := &Connection{messageChan: make(chan []byte)}
+	conn := &Connection{messageChan: make(chan Message)}
 	h.register <- conn
 	defer func() { h.unregister <- conn }()
+
+	closeNotifier := w.(http.CloseNotifier).CloseNotify()
+	go func() {
+		<-closeNotifier
+		h.unregister <- conn
+	}()
 
 	// declare SSE MIME type
 	w.Header().Set("Content-type", "text/event-stream")
 
 	for {
-		b.Write([]byte("data: ")) // SSE message header
-		b.Write(<-conn.messageChan)
-		b.Write([]byte("\n\n")) // SSE message coda
-
+		m := <-conn.messageChan
+		b.Write(m.Bytes())
 		b.WriteTo(w)
 		flusher.Flush()
 	}
@@ -118,8 +123,10 @@ func ClientHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	message := r.PostFormValue("message")
-	h.broadcast <- []byte(message)
+	messageText := r.PostFormValue("message")
+	h.broadcast <- Message{data: messageText}
+
+	fmt.Printf("Received message: %s\n", messageText)
 
 	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 	return
