02a254b62d22da5795c1b9d75fb98547424748d6
Ivan Tam <ivan@hipnik.net>
2015-02-12 21:28:28 -0800

    Adding exploratory JSON parsing code
    I needed to find something that would help me parse and
    navigate JSON.
    
    Saving this playground for further experiments.

---
+++ webhook.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jmoiron/jsonq"
+	"os"
+)
+
+func main() {
+	// generic data struct
+	data := map[string]interface{}{}
+
+	// load JSON string from file
+	file, e := os.Open("./json_samples/pullreq.json")
+	if e != nil {
+		fmt.Printf("Error opening file %v\n", e)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	// load and decode string into our data struct
+	dec := json.NewDecoder(file)
+	dec.Decode(&data)
+	jq := jsonq.NewQuery(data)
+
+	fmt.Println(jq.String("action"))
+	fmt.Println(jq.String("pull_request", "url"))
+	fmt.Println(jq.String("pull_request", "state"))
+	fmt.Println(jq.String("pull_request", "user", "login"))
+	fmt.Println(jq.String("pull_request", "user", "avatar_url"))
+}
